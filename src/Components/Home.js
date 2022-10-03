d911f4e4639abe9d08e5e8b8217dad8d7effecb9
thaisdavilla <thaisdavilla@yahoo.com.br>
2022-10-03 12:52:49 -0300

    Inicio do projeto. Home e About.

---
+++ src/Components/Home.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Photo from "../Images/photoHome.jpg";
+import GitHub from "../Images/gitHub.png";
+import Insta from "../Images/instagram.png";
+import In from "../Images/linkedin.png";
+
+export default function Home() {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Work</li>
+          <li>ODS</li>
+        </ul>
+      </nav>
+      <h1>Thais D'Ávilla</h1>
+      <img src={Photo} alt="Thais D'Ávilla em frente a um lago."/>
+      <div>
+        <a href="https://github.com/ThaisDavilla">
+          <img src={GitHub} alt="Ícone do GitHub"/>
+        </a>
+        <a href="https://www.instagram.com/athaisdavilla/">
+          <img src={Insta} alt="Ícone do Instagram"/>
+        </a>
+        <a href="https://www.linkedin.com/in/thaisdavilla01/">
+          <img src={In} alt="Ícone do Linkedin"/>
+        </a>
+      </div>
+    </>
+  )
+}
