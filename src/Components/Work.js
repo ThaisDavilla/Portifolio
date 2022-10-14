45f45097b5dd6e8a73d2abaaae59f3f11d8fc224
thaisdavilla <thaisdavilla@yahoo.com.br>
2022-10-14 10:26:38 -0300

    Construção da page Work.

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -16,75 +16,78 @@ export default function Work() {
       <header>
         <S.Hone>FrontEnd Development</S.Hone>
         <S.Htwo>Conheça um pouco do meu trabalho</S.Htwo>
-        {/*<S.Htree>HTML 5, CSS 3, Responsividade, JavaScripit, DOM, React, State, Styled Components,
-          Props, Hooks, Rest API.
-  </S.Htree>*/}
       </header>
       <S.Main>
         <S.Div>
           <S.Img src={Picture} alt="Imagem da página de login do jogo League Of Legend" />
-          <h3>Login Lague Of Legend</h3>
-          <p>Trabalho manipulando o DOM para interação de login.</p>
-          <a href="https://thaisdavilla.github.io/leagueOfLegend/" target="_blank">Clique aqui e conheça a página!</a>
+          <S.Htree>Login Lague Of Legend</S.Htree>
+          <S.Paragraph>Página interativa! Quando são preenchidos os campos acontece
+            a ativação do botão. Efeito conseguido através da manipulação do DOM.</S.Paragraph>
+          <S.Ancora href="https://thaisdavilla.github.io/leagueOfLegend/" target="_blank">Conheça a página!</S.Ancora>
         </S.Div>
         <S.Div>
-          <S.Img src={PictureOne} alt="Imagem da página Beat, no compço da batida" />
-          <h3>Beat, no compaço da batida</h3>
-          <p>Praticando a aplicação de flexBox.</p>
-          <a href="https://thaisdavilla.github.io/beat/" target="_blank">Clique aqui e conheça a página!</a>
+          <S.Img src={PictureOne} alt="Imagem da página Beat, no compaço da batida" />
+          <S.Htree>Beat, no compaço da batida</S.Htree>
+          <S.Paragraph>Desing reproduzido do figma, visando a estruturação e estilização aplicando Flex-Box.</S.Paragraph>
+          <S.Ancora href="https://thaisdavilla.github.io/beat/" target="_blank">Conheça a página!</S.Ancora>
         </S.Div>
         <S.Div>
           <S.Img src={PictureTwo} alt="Imagem da página PortFlow" />
-          <h3>PortiFlow - Imagine, Discuta, Desenhe.</h3>
-          <p>Design reproduzido do Figma.</p>
-          <a href="https://thaisdavilla.github.io/portFlow/" target="_blank">Clique aqui e conheça a página!</a>
+          <S.Htree>PortiFlow - Imagine, Discuta, Desenhe.</S.Htree>
+          <S.Paragraph>
+            Reprodução do Figma aplicando recursos aprendidos do HTML5, CSS3, semântica
+            e responsividade. Trabalho de conclusão do primeiro módulo do curso Vai na Web.
+          </S.Paragraph>
+          <S.Ancora href="https://thaisdavilla.github.io/portFlow/" target="_blank">Conheça a página!</S.Ancora>
         </S.Div>
         <S.Div>
           <S.Img src={PictureTree} alt="Imagem da Bio." />
-          <h3>Little Contact bio</h3>
-          <p>Construção de uma mini bio para prática de HTML e CSS.</p>
-          <a href="https://thaisdavilla.github.io/bioDevFrontEnd/" target="_blank">Clique aqui e conheça a página!</a>
+          <S.Htree>Little Contact bio</S.Htree>
+          <S.Paragraph>Fiz essa página para deixar o link como Bio nas minhas redes sociais.
+            Foi um trabalho divertido de fazer em um mini curso para prática de HTML e CSS. 
+          </S.Paragraph>
+          <S.Ancora href="https://thaisdavilla.github.io/bioDevFrontEnd/" target="_blank">Conheça a página!</S.Ancora>
         </S.Div>
         <S.Div>
           <S.Img src={PictureFour} alt="Imagem do Blog." />
-          <h3>Litlle Blog ThaisDavilla </h3>
-          <p>Este foi meu primeiro contato prático com códigos.
+          <S.Htree>Litlle Blog ThaisDavilla </S.Htree>
+          <S.Paragraph>Este foi meu primeiro contato prático com códigos.
             Esta página foi desenvolvida em uma oficina de 8h de um sábado.
             Foi neste dia que me apaixonei pelo FrontEnd.
-          </p>
-          <a href="https://thaisdavilla.github.io/litlleBlogThaisDavilla/" target="_blank">Clique aqui e conheça a página!</a>
+          </S.Paragraph>
+          <S.Ancora href="https://thaisdavilla.github.io/litlleBlogThaisDavilla/" target="_blank">Conheça a página!</S.Ancora>
         </S.Div>
         <S.Div>
           <S.Img src={PictureFive} alt="Imagem da página da Ada Lovelace." />
-          <h3>Site Ada Lovelace</h3>
-          <p>Fiz esta página num curso introdutório de programação.
+          <S.Htree>Site Ada Lovelace</S.Htree>
+          <S.Paragraph>Fiz esta página num curso introdutório de programação.
             Além do HTML e do CSS tem um pouco de JavaScripit para interação do usuário.
-          </p>
-          <a href="" target="_blank">Clique aqui e conheça a página!</a>
+          </S.Paragraph>
+          <S.Ancora href="" target="_blank">Conheça a página!</S.Ancora>
         </S.Div>
         <S.Div>
-          <S.Img src={PictureSix} alt="Imagem da página da Ada Lovelace." />
-          <h3>Site Ada Lovelace</h3>
-          <p>Fiz esta página num curso introdutório de programação.
-            Além do HTML e do CSS tem um pouco de JavaScripit para interação do usuário.
-          </p>
-          <a href="" target="_blank">Clique aqui e conheça a página!</a>
+          <S.Img src={PictureSix} alt="Imagem da página sobre ODS." />
+          <S.Htree>Objetivos de Desenvolvimento Sustetável</S.Htree>
+          <S.Paragraph>Pesquisa sobre quais as medidas que a cidade de Mogi das Cruzes tem tomado para
+            colaborar e atingir as metas acordadas com a ONU.
+          </S.Paragraph>
+          <S.Ancora href="https://ods-smoky.vercel.app/" target="_blank">Conheça a página!</S.Ancora>
         </S.Div>
         <S.Div>
           <S.Img src={PictureSeven} alt="Imagem da página da Ada Lovelace." />
-          <h3>Site Ada Lovelace</h3>
-          <p>Fiz esta página num curso introdutório de programação.
-            Além do HTML e do CSS tem um pouco de JavaScripit para interação do usuário.
-          </p>
-          <a href="" target="_blank">Clique aqui e conheça a página!</a>
+          <S.Htree>Rick and Morty API</S.Htree>
+          <S.Paragraph>Esse trabalho foi feito em grupo durante a aula para que pudéssemos 
+            praticar o consumo e a estilização de API rest.
+          </S.Paragraph>
+          <S.Ancora href="https://rick-and-morty-black-ten.vercel.app/" target="_blank">Conheça a página!</S.Ancora>
         </S.Div>
         <S.Div>
-        <S.Img src={PictureEight} alt="Imagem da página da Ada Lovelace." />
-          <h3>Site Ada Lovelace</h3>
-          <p>Fiz esta página num curso introdutório de programação.
-            Além do HTML e do CSS tem um pouco de JavaScripit para interação do usuário.
-          </p>
-          <a href="" target="_blank">Clique aqui e conheça a página!</a>
+          <S.Img src={PictureEight} alt="Imagem da página da Ada Lovelace." />
+          <S.Htree>Adote um Amicão!</S.Htree>
+          <S.Paragraph>Página para prática de consumo de API, simulando um site de adoção de cães.
+            *Estilização e responsividade da página não estão construidas.*
+          </S.Paragraph>
+          <S.Ancora href="https://adote-api.vercel.app/" target="_blank">Conheça a página!</S.Ancora>
         </S.Div>
       </S.Main>
     </S.Body>
--- src/Style/WorkStyle.js
+++ src/Style/WorkStyle.js
@@ -20,9 +20,9 @@ justify-content: center;
 font-family:'Nanum Gótico', sans-serif;
 color: #52796f;
 @media screen and (min-width: 360px) and (max-width: 800px) {
-  width: 60vw;
-  margin:1vh auto;
-  font-size: 1.5em;
+  width: 80vw;
+  margin:0vh auto;
+  font-size: 1.4em;
 `
 export const Htwo = styled.h2`
 width: 90vw;
@@ -33,43 +33,74 @@ justify-content: center;
 font-family:'Nanum Gótico', sans-serif;
 color: #52796f;
 @media screen and (min-width: 360px) and (max-width: 800px) {
-  width: 60vw;
-  margin:1vh auto;
-  font-size: 1.5em;
+  width: 90vw;
+  margin:2vh auto;
+  font-size: 1.2em;
 `
-export const Htree = styled.h2`
-width: 93vw;
-font-size: 1.3em;
-margin: 2vh auto;
-display: flex;
-justify-content: center;
-font-family:'Nanum Gótico', sans-serif;
-color: #52796f;
-@media screen and (min-width: 360px) and (max-width: 800px) {
-  width: 60vw;
-  margin:1vh auto;
-  font-size: 1em;
-`
+
 export const Main = styled.main`
   width:97vw;
-  height:100vh;
+  height:100%;
   margin: 0 auto;
+  color:#2f3e46;
   display: flex;
   justify-content:space-evenly;
   flex-wrap: wrap;
   border:;
+  @media screen and (min-width: 360px) and (max-width: 800px){
+    width:95vw;
+  height:100%;
+  margin: 2vh auto;
+  }
 `
 export const Div = styled.div`
   width:30vw;
-  height:48vh;
+  height:55vh;
+  margin:1.2vh;
   display:flex;
   align-intens:center;
   justify-content:center;
   flex-wrap: wrap;
-  border:;
+  @media screen and (min-width: 360px) and (max-width: 800px){
+  width:90vw;
+  height:55vh;
+  margin:1vh;
+  display:flex;
+  align-intens:center;
+  justify-content:center;
+  flex-wrap: wrap;
+  
+  }
 `
 export const Img = styled.img`
 width:100%;
 height:30vh;
-border: 1px solid;
+border: 1px solid #4a4e69;
+@media screen and (min-width: 360px) and (max-width: 800px){
+width:100%;
+height:30vh;
+}
+`
+export const Htree = styled.h3`
+font-size:1em;
+@media screen and (min-width: 360px) and (max-width: 800px){
+
+}
+`
+export const Paragraph = styled.p`
+font-size:1em;
+width:50vh;
+margin:1vh auto;
+@media screen and (min-width: 360px) and (max-width: 800px){
+  font-size:1em;
+  width:40vh;
+  margin:0.5vh auto;
+
+}
+
+
+`
+export const Ancora = styled.a`
+text-decoration:none;
+color:#2f3e46;
 `
